3aef4d925ba1f7e398c19baa0100eac6c59f5b1d
kurta17 <114169244+kurta17@users.noreply.github.com>
2023-10-03 00:45:30 +0700

    add 3 task

--- 2.py
+++ 2.py
@@ -8,26 +8,65 @@ def send_sms(msg, phone):
     """
     ...
 
-
 def send_email(msg, email):
     """Sends a given message to a given address as email.
 
     You don't have to implement this function.
     """
     ...
-
 # Solution goes here
+
+class Patient:
+    def __init__(self, name, family_name, sex, birthday, gmail, number1, number2):
+        self.name = name
+        self.family_name = family_name
+        self.sex = sex
+        self.birthday = birthday
+        self.gmail = gmail
+        self.number1 = number1
+        self.number2 = number2
+
+    def greeting(self):
+        return f'Hello {self.sex} {self.name} {self.family_name} !'
+
+    def age(self):
+        now = datetime.now()
+        your_age = now.year - self.birthday.year
+        your_month = now.month - self.birthday.month
+        your_day = now.day - self.birthday.day
+        if your_month < 0:
+            your_month = int(your_month) * -1
+            your_age = your_age -1
+        if your_day < 0:
+            your_day = your_day * -1
+            your_month = your_month -1
+            if your_month < 0:
+                your_month = int(your_month) * -1
+                your_age = your_age -1
+
+        return f'{your_age}Y {your_month}M {your_day}D'
+    
+    def send_appointment_reminder(self, date):
+        self.date = date
+        msg = f'I send an appointment reminder letter. Your appointment will be {self.date}'
+        send_email(msg , self.gmail)
+        send_sms(msg, self.number1)
+        send_sms(msg, self.number2)
+        return msg
+
+
+        
+
 
 patient1 = Patient(
     'John',
     'Doe',
     'mr.',
-    datetime(1990, 2, 28),
+    datetime(1994, 2, 25),
     'john.doe@gmail.com',
     '+3345451555',
     '+6693932030',
 )
-
 print(patient1.greeting())
-
-patient1.send_appointment_reminder(datetime(2023, 10, 13, 13, 0))
+print(patient1.age())
+print(patient1.send_appointment_reminder(datetime(2023, 10, 13, 13, 0)))
