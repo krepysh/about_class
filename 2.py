6741931e236ff0cfe7a6791375b2dae563032691
pankace <44595774+pankace@users.noreply.github.com>
2023-10-02 12:18:51 +0700

    dsjfsjkooooooooo

--- 2.py
+++ 2.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 
 def send_sms(msg, phone):
-    """Sends a given message to a given phone via SMS.
+    """Sends a given message to a given phone number as SMS.
 
     You don't have to implement this function.
     """
@@ -21,14 +21,23 @@ class Patient(object):
         self.first_name = first_name
         self.last_name = last_name
         self.title = title
-        self.date = date
+        self.age = datetime.now() - date
         self.email = email
         self.phone = phone
-        self.moble_phone = mobile_phone
+        self.mobile_phone = mobile_phone
 
     def greeting(self):
         return f"Hello, {self.title} {self.last_name}!"
+    
+    def send_sms(self, msg):
+        send_sms(msg, self.mobile_phone)
+    
+    def send_email(self, msg):
+        send_email(msg, self.email)
 
+    def send_appointment_reminder(self, time):
+        send_sms(f"Hi {self.first_name}, you have an appointment tomorrow at {time}.", self.mobile_phone)
+        send_email(f"Hi {self.first_name}, you have an appointment tomorrow at {time}.", self.email)
 
 patient1 = Patient(
     "John",
@@ -43,4 +52,4 @@ patient1 = Patient(
 
 print(patient1.greeting())
 
-#patient1.send_appointment_reminder(datetime(2023, 10, 13, 13, 0))
+patient1.send_appointment_reminder(datetime(2023, 10, 13, 13, 0))
