625a3f56a2d2018b9187856b6616d4247b157167
pankace <44595774+pankace@users.noreply.github.com>
2023-10-02 11:42:48 +0700

    this coffeeeeeeeeeeeeeeeeeeeeeeeeee

--- 2.py
+++ 2.py
@@ -16,18 +16,30 @@ def send_email(msg, email):
     """
     ...
 
-# Solution goes here
+class Patient(object):
+    def __init__(self, first_name, last_name, title, date, email, phone, mobile_phone):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.title = title
+        self.date = date
+        self.email = email
+        self.phone = phone
+        self.moble_phone = mobile_phone
+
+    def greeting(self):
+        return f"Hello, {self.title} {self.last_name}!"
+
 
 patient1 = Patient(
-    'John',
-    'Doe',
-    'mr.',
+    "John",
+    "Doe",
+    "mr.",
     datetime(1990, 2, 28),
-    'john.doe@gmail.com',
-    '+3345451555',
-    '+6693932030',
+    "john.doe@gmail.com",
+    "+3345451555",
+    "+6693932030",
 )
 
 print(patient1.greeting())
 
-patient1.send_appointment_reminder(datetime(2023, 10, 13, 13, 0))
+#patient1.send_appointment_reminder(datetime(2023, 10, 13, 13, 0))
