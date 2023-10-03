137ed3157ff0770e8f94b40553d076b15d4c0dd8
pankace <44595774+pankace@users.noreply.github.com>
2023-10-03 09:49:57 +0700

    oompaloompa

--- oop-coffee-machine-start/oop-coffee-machine-start/main.py
+++ oop-coffee-machine-start/oop-coffee-machine-start/main.py
@@ -2,6 +2,7 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
+
 def __main__():
     coffee_maker = CoffeeMaker()
     menu = Menu()
@@ -18,8 +19,11 @@ def __main__():
             money_machine.report()
         else:
             drink = menu.find_drink(choice)
-            if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            if coffee_maker.is_resource_sufficient(
+                drink
+            ) and money_machine.make_payment(drink.cost):
                 coffee_maker.make_coffee(drink)
+
 
 if __name__ == "__main__":
     __main__()
