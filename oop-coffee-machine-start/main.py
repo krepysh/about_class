4ce6e45f648cd6b98db387e89f1e79ca371d4ec9
storm <stormurbiz@gmail.com>
2023-10-03 10:07:01 +0700

    class caufey

---
+++ oop-coffee-machine-start/main.py
@@ -0,0 +1,28 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+
+
+is_on = True
+
+while is_on:
+    choice = input("What would you like? (espresso/latte/cappuccino): ")
+    if choice == 'off':
+        exit()
+    elif choice == 'report':
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        if drink:
+            if coffee_maker.is_resource_sufficient(drink):
+                if money_machine.make_payment(drink.cost):
+                    coffee_maker.make_coffee(drink)
+    
+
+
